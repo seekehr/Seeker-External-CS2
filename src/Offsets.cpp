e2d3496fe9c08ab19cf5a133a8135ca38016ffc3
seekehr <grouchyseeker@gmail.com>
2026-03-20 14:21:11 +0500

    load offsets from offsets/ or ../offsets/

--- src/Offsets.cpp
+++ src/Offsets.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <iterator>
 #include <string>
+#include <vector>
+#include <Windows.h>
 
 namespace {
 bool ReadTextFile(const std::string& path, std::string& out) {
@@ -42,37 +44,60 @@ bool ParseUIntAfterKey(const std::string& content, const char* key, uintptr_t& o
 }
 }
 
-bool Offsets::LoadFromFolder(const std::string& folderPath) {
+static bool LoadFromSingleFolder(const std::string& folderPath) {
     bool anyLoaded = false;
 
     std::string offsetsJson;
     if (ReadTextFile(folderPath + "/offsets.json", offsetsJson)) {
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwEntityList", Client::dwEntityList, false);
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwLocalPlayerController", Client::dwLocalPlayerController, false);
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwLocalPlayerPawn", Client::dwLocalPlayerPawn, false);
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwViewMatrix", Client::dwViewMatrix, false);
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwCSGOInput", Client::dwCSGOInput, false);
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwWindowWidth", Engine::dwWindowWidth, false);
-        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwWindowHeight", Engine::dwWindowHeight, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwEntityList", Offsets::Client::dwEntityList, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwLocalPlayerController", Offsets::Client::dwLocalPlayerController, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwLocalPlayerPawn", Offsets::Client::dwLocalPlayerPawn, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwViewMatrix", Offsets::Client::dwViewMatrix, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwCSGOInput", Offsets::Client::dwCSGOInput, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwWindowWidth", Offsets::Engine::dwWindowWidth, false);
+        anyLoaded |= ParseUIntAfterKey(offsetsJson, "dwWindowHeight", Offsets::Engine::dwWindowHeight, false);
     }
 
     std::string buttonsJson;
     if (ReadTextFile(folderPath + "/buttons.json", buttonsJson)) {
-        anyLoaded |= ParseUIntAfterKey(buttonsJson, "jump", Buttons::jump, false);
+        anyLoaded |= ParseUIntAfterKey(buttonsJson, "jump", Offsets::Buttons::jump, false);
     }
 
     std::string clientDllJson;
     if (ReadTextFile(folderPath + "/client_dll.json", clientDllJson)) {
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_hPlayerPawn", CCSPlayerController::m_hPlayerPawn, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_iHealth", C_BaseEntity::m_iHealth, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_iTeamNum", C_BaseEntity::m_iTeamNum, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_fFlags", C_BaseEntity::m_fFlags, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_pGameSceneNode", C_BaseEntity::m_pGameSceneNode, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_vOldOrigin", C_BaseEntity::m_vOldOrigin, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_vecViewOffset", C_BaseEntity::m_vecViewOffset, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_iIDEntIndex", C_CSPlayerPawnBase::m_iIDEntIndex, true);
-        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_vecAbsOrigin", CGameSceneNode::m_vecAbsOrigin, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_hPlayerPawn", Offsets::CCSPlayerController::m_hPlayerPawn, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_iHealth", Offsets::C_BaseEntity::m_iHealth, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_iTeamNum", Offsets::C_BaseEntity::m_iTeamNum, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_fFlags", Offsets::C_BaseEntity::m_fFlags, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_pGameSceneNode", Offsets::C_BaseEntity::m_pGameSceneNode, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_vOldOrigin", Offsets::C_BaseEntity::m_vOldOrigin, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_vecViewOffset", Offsets::C_BaseEntity::m_vecViewOffset, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_iIDEntIndex", Offsets::C_CSPlayerPawnBase::m_iIDEntIndex, true);
+        anyLoaded |= ParseUIntAfterKey(clientDllJson, "m_vecAbsOrigin", Offsets::CGameSceneNode::m_vecAbsOrigin, true);
     }
 
     return anyLoaded;
+}
+
+bool Offsets::LoadFromFolder(const std::string& folderPath) {
+    std::vector<std::string> candidates;
+    candidates.push_back(folderPath);
+    candidates.push_back("./offsets");
+    candidates.push_back("../offsets");
+
+    char modulePath[MAX_PATH] = {0};
+    if (GetModuleFileNameA(nullptr, modulePath, MAX_PATH) > 0) {
+        std::string exePath(modulePath);
+        size_t sep = exePath.find_last_of("\\/");
+        if (sep != std::string::npos) {
+            const std::string exeDir = exePath.substr(0, sep);
+            candidates.push_back(exeDir + "/offsets");
+            candidates.push_back(exeDir + "/../offsets");
+        }
+    }
+
+    for (const auto& candidate : candidates) {
+        if (LoadFromSingleFolder(candidate)) return true;
+    }
+    return false;
 }
